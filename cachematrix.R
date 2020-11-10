eefd51fb926b6f495ec6e56c66e30d47fe682d75
sevdeetoz <sevdeetoz@gmail.com>
2020-11-09 22:21:29 -0500

    I added the code within the function bracets.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function keeps the data and its inverse if exist. 
+## Nested functions (get, set etc..) communicates matrices with the cacheSolve
 
-## Write a short comment describing this function
+## makeCacheMatrix keeps cached data and inverse of data matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## If the inverse in makeCacheMatrix is not NULL, it won't be calculated here
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
